e0f92045362dd9fddc01088cdd527b60aaa38577
Barney <connor.barney@me.com>
2023-07-20 19:01:57 +0800

    Barracade Damage Fix

--- Objects/barracade.gd
+++ Objects/barracade.gd
@@ -9,15 +9,27 @@ var im_a_barracade = true
 var active = false
 var health = 6
 
+var shake_time = 0
+var default_x_pos = 0
+
 var particles_object = preload("res://Objects/particles.tscn")
 
 func _ready():
 	create_particle_explosion(Color.WHITE_SMOKE)
+	default_x_pos = position.x
+	$Sprite.frame = 0
+
+func _physics_process(_delta):
+	if shake_time > 0:
+		position.x = default_x_pos + sin(Time.get_ticks_msec()) * shake_time * 0.4
+		shake_time -= 1
 
 func barracade_tick():
 	if active:
 		health -= 1
+		$Sprite.frame = (6 - (health - 1))/2
 		GlobalStats.play_sound("Hurt")
+		shake_time += 10
 		if health <= 0:
 			create_particle_explosion(Color.SANDY_BROWN)
 			GlobalStats.play_sound("Death")
